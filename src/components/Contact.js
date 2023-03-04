3ab5817ce6441989f13fdcae731428cf1847b710
OdilbekDeveloper <desmon.programmer@gmail.com>
2023-03-05 00:15:55 +0500

    '1'

---
+++ src/components/Contact.js
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Contact = () => {
+  const formInitialDetails = {
+    first_name: '',
+    last_name: '',
+    phone: '',
+    message: ''
+  }
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState('Send');
+  const [status, setStatus] = useState({});
+
+  const onFormUpdate = (category, value) => {
+      setFormDetails({
+        ...formDetails,
+        [category]: value
+      })
+  }
+
+  const dataform = new FormData()
+  dataform.append('first_name', formDetails.first_name)
+  dataform.append('last_name', formDetails.last_name)
+  dataform.append('phone', formDetails.phone)
+  dataform.append('message', formDetails.message)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+    let response = await fetch("http://127.0.0.1:8000/api/contact/", {
+      method: "POST",
+      body: dataform,
+    });
+    setButtonText("Send");
+    let result = await response.json();
+    setFormDetails(formInitialDetails);
+    if (result.code == 200) {
+      setStatus({ succes: true, message: 'Message sent successfully'});
+    } else {
+      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
+    }
+  };
+
+  useEffect(() => {
+    console.log('changed');
+  }, onFormUpdate)
+
+
+  return (
+    <section className="contact" id="connect">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+              }
+            </TrackVisibility>
+          </Col>
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <h2>Get In Touch</h2>
+                <form onSubmit={handleSubmit}>
+                  <Row>
+                    <Col size={12} sm={6} className="px-1">
+                      <input type="text" value={formDetails.first_name} placeholder="First Name" onChange={(e) => onFormUpdate('first_name', e.target.value)} />
+                    </Col>
+                    <Col size={12} sm={6} className="px-1">
+                      <input type="text" value={formDetails.last_name} placeholder="Last Name" onChange={(e) => onFormUpdate('last_name', e.target.value)}/>
+                    </Col>
+                    <Col size={12} sm={6} className="px-1">
+                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                    </Col>
+                    <Col size={12} sm={6} className="px-1">
+                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                    </Col>
+                    <Col size={12} className="px-1">
+                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                      <button type="submit"><span>{buttonText}</span></button>
+                    </Col>
+                    {
+                      status.message &&
+                      <Col>
+                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                      </Col>
+                    }
+                  </Row>
+                </form>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
